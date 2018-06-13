de7a72ad0f38d0036e99dc892c6c066349020cb9
andmichalski <andrzmichal@gmail.com>
2018-06-13 07:38:22 +0200

    Initialized project

--- todo/admin.py
+++ todo/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+
+@admin.register(Task)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ["title", "text", "status"]
--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [("todo", "todo"), ("in progress", "in progress"), ("done", "done"), ("to delete", "to delete")]
+
+    title = models.CharField(max_length=100)
+    text = models.TextField(max_length=1000)
+    status = models.CharField(max_length=11, choices=STATUS_CHOICES)
